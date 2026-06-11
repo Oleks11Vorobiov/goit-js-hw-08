06b31e1566134677cea066dd402ad132aaf0f6f1
Oleks11Vorobiov <19aleksey.v.91@gmail.com>
2026-06-11 21:49:07 +0300

    feat: implement color switcher logic

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButtonRef = document.querySelector('[data-start]');
+const stopButtonRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startButtonRef.addEventListener('click', onStartButtonClick);
+stopButtonRef.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+  startButtonRef.disabled = true;
+
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  startButtonRef.disabled = false;
+}
